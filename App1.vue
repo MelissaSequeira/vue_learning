52aef8805ccf0294c896b6ff1848c2e49818cd0b
melody2004 <154119563+MelissaSequeira@users.noreply.github.com>
2026-08-14 13:54:32 +0530

    Add checkbox for terms acceptance in App1.vue

---
+++ App1.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+  const radio=ref<boolean>(false)
+</script>
+<template>
+  <form @submit.prevent>
+    <p><input v-model="radio" type="checkbox"></input>Accept the terms and conditions</p>
+  </form>
+  <p v-if="radio">Continue for more updates!!</p>
+  <p v-else>to Continue accept the terms and conditions</p>
+</template>
